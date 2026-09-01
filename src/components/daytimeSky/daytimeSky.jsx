441361ab9a9b6f5e22b45a557fa1ab76c5d4b864
solsticiodeverano <talleresfinge@gmail.com>
2026-09-01 13:30:42 -0300

    cambios

--- src/components/daytimeSky/daytimeSky.jsx
+++ src/components/daytimeSky/daytimeSky.jsx
@@ -76,7 +76,6 @@ const DaytimeSky = ({ prestacionAlimentaria, raciones }) => {
       >
         {icon} {/* Always show the icon */}
       </div>
-      {raciones && <div className={styles.raciones}> <h6> Raciones<h6/></h6> {raciones}</div>} {/* Show raciones only if it has a value */}
     </section>
   );
 };
